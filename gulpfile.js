f6a55ba1b1811041bb9cbbac856bd1419afe0fa4
jvpcavalcante <joao_vitor_pedrosa@hotmail.com>
2016-09-20 01:16:33 -0300

    teste rv

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "dev"
+    });
+
+    gulp.watch("dev/scss/*.scss", ['sass']);
+    gulp.watch("dev/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("dev/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("dev/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
